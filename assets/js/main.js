cabc0fdb9961eb630fb1d0a76ed1c036466a4b3a
athena <athena.nguyen92@gmail.com>
2025-04-29 11:59:20 +0900

    edit job

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,9 +93,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 110,
+      backSpeed: 20,
+      backDelay: 1500
     });
   }
 
